02ac92e3b3233a58c4a91dcb8969ab9aedc83eb2
mateussousaa <mateussousa20@outlook.com>
2025-04-17 23:47:04 -0300

    feat: creating the department entity

---
+++ src/model/entities/Department.java
@@ -0,0 +1,50 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private int id;
+    private String name;
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
